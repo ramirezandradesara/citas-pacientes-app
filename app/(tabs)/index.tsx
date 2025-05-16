d81819b4d3f369de2fdef9ce2d63c7ba8e1c14a7
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2025-05-16 16:19:44 -0300

    create new home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,68 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useState } from "react";
+import { Pressable, SafeAreaView, StyleSheet, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Formulario from "../../components/Formulario";
 
 export default function HomeScreen() {
+  const [clientes, setClientes] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const nuevaCitaHandler = () => {
+    console.log("nuevaCitaHandler clicked");
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <GestureHandlerRootView>
+      <SafeAreaView style={styles.contenedor}>
+        <Text style={styles.titulo}>Hola Mundo!</Text>
+        <Text style={styles.tituloBold}>Soy Sari</Text>
+        {/* <Button onPress={() => console.log("Press")} title="Nueva cita" /> */}
+        <Pressable
+          onPress={() => setModalVisible(true)}
+          style={styles.btnNuevaCita}
+        >
+          <Text style={styles.btnTextoNuevaCita}>Nueva cita</Text>
+        </Pressable>
+        <Formulario
+          modalVisible={modalVisible}
+          nuevaCitaHandler={nuevaCitaHandler}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  contenedor: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "grey",
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  titulo: {
+    textAlign: "center",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "white",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  tituloBold: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "purple",
+  },
+  btnNuevaCita: {
+    backgroundColor: "#6D28D9",
+    marginRight: 5,
+    marginHorizontal: 5,
+    borderRadius: 10,
+    padding: 15,
+  },
+  btnTextoNuevaCita: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: 900,
+    textTransform: "uppercase",
   },
 });
